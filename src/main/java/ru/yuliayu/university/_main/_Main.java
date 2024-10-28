0731370bd72098bcfc943516cd44d05adf028def
Yulia <yuliayugaldina@gmai.com>
2024-10-28 20:42:16 +0500

    Init class _Main method main.

--- src/main/java/ru/yuliayu/university/_main/_Main.java
+++ src/main/java/ru/yuliayu/university/_main/_Main.java
@@ -1,7 +1,32 @@
 package ru.yuliayu.university._main;
 
+import ru.yuliayu.university.entity.Student;
+import ru.yuliayu.university.entity.Teacher;
+import ru.yuliayu.university.entity.University;
+import ru.yuliayu.university.util.StudentExecutor;
+import ru.yuliayu.university.util.TeacherExecutor;
+
 public class _Main {
     public static void main (String[] args){
+        University university = new University("ДВФУ", 1895);
+        Teacher teacher = new Teacher("Крестникова Ольга Александровна", 35, "Основы алгоритмизации и программирования");
+        Student student = new Student("Югалдина Юлия Константиновна", 22, "программная инженерия");
+        university.setTeacher(teacher);
+        university.setStudent(student);
+        teacher.setUniversity(university);
+        teacher.setStudent(student);
+        student.setTeacher(teacher);
+        student.setUniversity(university);
+        System.out.println("Студент:");
+        StudentExecutor studentExecutor = new StudentExecutor();
+        studentExecutor.makeEat(student);
+        studentExecutor.makeSleep(student);
+        studentExecutor.makeSkipClass(student);
+        System.out.println("\nПреподователь:");
+        TeacherExecutor teacherExecutor = new TeacherExecutor();
+        teacherExecutor.makeEat(teacher);
+        teacherExecutor.makeSleep(teacher);
+        teacherExecutor.makeSubmitReport(teacher);
 
     }
 }
